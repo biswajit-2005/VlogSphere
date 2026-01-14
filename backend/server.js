d621b43016487efe5c49bc687baf53eb8bed9955
Biswajit Baliarsingh <biswajitbaliarsingh777@gmail.com>
2026-01-15 02:39:40 +0530

    Backend is Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ const connectToDb = require("./config/db.js");
 dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use("/api", vlogRoutes);
 app.listen(port, () => {
